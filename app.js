5546b172327c7383b4d9669c409c0a970f3b7c31
skycoder999 <amreensultan999@gmail.com>
2021-02-14 17:36:25 +0530

    feat(backend): mongodb connection

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+// app
+
+const app = express();
+
 app.get("/", (req, res) => {
-	res.send("hello from node");
+	res.send("hello from amreen");
 });
+
+// db
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Database connected successfully");
+	})
+	.catch((e) => {
+		console.log(e);
+	});
 
 const port = process.env.PORT || 8000;
 
